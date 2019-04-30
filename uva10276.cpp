a869987603768a6d7e8133c85a903dd0703f5c79
rast-7 <rajatasthana4@gmail.com>
2019-04-30 23:38:15 +0530

    UVA 10276 correct solution

--- uva10276.cpp
+++ uva10276.cpp
@@ -3,38 +3,41 @@ using namespace std;
 
 bool is_square_number(int x)
 {
-	double sr = sqrt(x);
-	return ((sr - floor(sr)) == 0);
+        double sr = sqrt(x);
+        return ((sr - floor(sr)) == 0);
 }
 
 int main(void)
 {
-	int t;
-	cin >> t;
-	while(t--)
-	{
-		int n, i(1), j(0);
-		cin >> n;
-		if((n % 10) == 0)
-		{
-			cout << "-1\n";
-			continue;
-		}
-		vector< vector<int> > v(n);
-		while(1)
-		{
-			int flag(0);
-			for(int j=0; j<n; j++)
-			{
-				if((v[j].size() == 0) || (is_square_number(v[j][v[j].size()-1] + i)))
-				{
-					v[j].push_back(i);
-					flag = 1;
-				}
-			}
-			if(flag == 1)
-				break;
-		}
-		cout << i << "\n";
-	}
+        int t;
+        cin >> t;
+        while(t--)
+        {
+                int n, i(1), j(0);
+                cin >> n;
+                vector< vector<int> > v(n);
+                while(1)
+                {
+                        int flag(0), j(0);
+                        while(1)
+                        {
+                            if((v[j].size() == 0) || (is_square_number(v[j][v[j].size()-1] + i)))
+                            {
+                                    v[j].push_back(i);
+                                    flag = 1;
+                                    i++;
+                                    j = 0;
+                                    break;
+                            }
+                            j = (j+1)%(n);
+                            if((j == 0) && (flag == 0))
+                        		break;
+                        }
+                        if(flag == 0)
+                                break;
+                               
+                }
+                cout << i-1 << "\n";
+        }
 }
+
